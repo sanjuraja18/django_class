5c9566229200abc74847f0918832686e27f4eadd
Sanju chauhan <sanjuraja1805@gmail.com>
2025-01-11 15:33:37 +0530

    re

--- routing2/project/project/settings.py
+++ routing2/project/project/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+
+SANJUDIRS =os.path.join(BASE_DIR,'sanju')
+print(SANJUDIRS)
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +60,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [SANJUDIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

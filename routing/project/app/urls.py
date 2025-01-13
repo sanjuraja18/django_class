d7943793677c812d9931b0b4c13c89e7a34387cd
Sanju chauhan <sanjuraja1805@gmail.com>
2025-01-13 18:32:43 +0530

    static

--- routing/project/app/urls.py
+++ routing/project/app/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.Landingpage, name='Landingpage'),
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('search/', views.search, name='search'),
     
 ]
--- routing/project/app/views.py
+++ routing/project/app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
-def Landingpage(request):
+def home(request):
     return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def search(request):
+    return render(request,'search.html')

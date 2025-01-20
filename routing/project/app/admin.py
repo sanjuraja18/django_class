718d9725cbcf6de2305e4684e37ae2834e97b954
Sanju chauhan <sanjuraja1805@gmail.com>
2025-01-20 16:25:26 +0530

    121

--- routing/project/app/admin.py
+++ routing/project/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile,StudentData
+from .models import UserProfile,StudentData,AdminPage
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(StudentData)
+admin.site.register(AdminPage)
--- routing/project/app/models.py
+++ routing/project/app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class UserProfile(models.Model):
+    adharno=models.IntegerField(unique=True)
     sno=models.AutoField(primary_key=True)
     username=models.CharField(max_length=30,null=True,unique=True,db_index=True,blank=False,help_text="Enter a unique username")
     email=models.EmailField(max_length=255,unique=True,blank=False,db_index=True)
@@ -14,10 +15,25 @@ class UserProfile(models.Model):
     
 class StudentData(models.Model):
     sno=models.AutoField(primary_key=True)
+    adharno=models.OneToOneField(UserProfile,on_delete=models.PROTECT,)
     username=models.CharField(max_length=30,null=True,unique=True,db_index=True,blank=False,help_text="Enter a unique username")
     email=models.EmailField(max_length=255,unique=True,blank=False,db_index=True)
     bio=models.CharField(max_length=50,blank=True,null=True,db_index=True,help_text="Write a short bio about yourself")
     is_active=models.BooleanField(default=False,db_index=True)
     quail=[('b-tech','b-tech'),('m,tech','m-tech')]
     qulification=models.CharField(max_length=100,choices=quail,null=True,verbose_name='Quali',db_index=True)
+    def __str__(self):
+        return str(self.adharno)
+
+       
+
+
+class AdminPage(models.Model):
+    sno=models.AutoField(primary_key=True)
+    adminname=models.CharField(max_length=30,null=True,unique=True,db_index=True,blank=False,help_text="Enter a unique username")
+    email=models.EmailField(max_length=255,unique=True,blank=False,db_index=True)
+    bio=models.CharField(max_length=50,blank=True,null=True,db_index=True,help_text="Write a short bio about yourself")
+    is_active=models.BooleanField(default=False,db_index=True)
+    def __str__(self):
+        return (self.adminname)
     

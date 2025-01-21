10b2438a074b34afd1babf34c2e49f84f360634e
Sanju chauhan <sanjuraja1805@gmail.com>
2025-01-21 16:38:16 +0530

    OnetoMany

--- routing/project/app/admin.py
+++ routing/project/app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile,StudentData,AdminPage
+from .models import UserProfile,StudentData,AdminPage,DepartmentModel,Student
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(StudentData)
 admin.site.register(AdminPage)
+admin.site.register(DepartmentModel)
+admin.site.register(Student)
--- routing/project/app/models.py
+++ routing/project/app/models.py
@@ -34,6 +34,22 @@ class AdminPage(models.Model):
     email=models.EmailField(max_length=255,unique=True,blank=False,db_index=True)
     bio=models.CharField(max_length=50,blank=True,null=True,db_index=True,help_text="Write a short bio about yourself")
     is_active=models.BooleanField(default=False,db_index=True)
-    def __str__(self):
-        return (self.adminname)
+
+
+
     
+department=[("CSE","CSE"),("EXE","EXE"),("IT","IT"),("ME","ME"),("MI/MI","MI/MI")]
+class DepartmentModel(models.Model):
+    dep_name=models.CharField(max_length=100,verbose_name="name", choices=department, unique=True)
+    description=models.TextField(max_length=200,verbose_name="Desc")
+    def __str__(self):
+        return str(self.dep_name)
+
+Cources=[("1","B.Tech"),("2","B.Com"),("3","M.Com"),("4","BCA"),("5","M.Tech")]
+class Student(models.Model):
+    name=models.CharField(max_length=100,verbose_name="name", choices=Cources)
+    age=models.IntegerField()
+    email=models.EmailField()
+    stu_name=models.ForeignKey(DepartmentModel,on_delete=models.PROTECT, to_field='dep_name')
+    def __str__(self):
+        return str(self.stu_name)
